34409ee062ec71326e3b5cd5ecde407da1f4177f
TahaAlperenDoğan <150174413+TahaAlperenDogan@users.noreply.github.com>
2024-11-26 18:05:14 +0300

    DGD303-Midterm-Push
    First update push for the project.

---
+++ Avebo_ThePtarmigod/Assets/Scripts/Boss_Scripts_Health_CommanderHamster.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+    public int maxHealth = 100; // Boss'un maksimum caný
+    public int currentHealth; // Boss'un mevcut caný
+
+    void Start()
+    {
+        currentHealth = maxHealth; // Baþlangýçta maksimum cana sahip
+    }
+    
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage; // Hasarý mevcut candan çýkar
+        Debug.Log("Boss Health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Boss defeated!");
+        Destroy(gameObject); // Boss'u yok et
+    }
+
+    // Opsiyonel: Saðlýk göstergesi için
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 20), "Boss Health: " + currentHealth);
+    }
+}
